f739761b2f1bc60f235e0881e18c8659c89520d1
leafdesign33-coder <leafdesign25@gmail.com>
2026-01-29 13:29:58 +0100

    Create initial database structure in index.js
    Initialize database structure and log to console.

---
+++ index.js
@@ -0,0 +1,14 @@
+(function() {
+  const db = [
+    {
+      name: "minecraftworlds",
+      nav: [
+        { name: "Home" },
+        { name: "About" },
+        { name: "Contact" }
+      ]
+    }
+  ];
+
+  console.log(db);
+})();
